d065657bce08b5b4f4efc521f7b4b12927636b4e
Jared Odulio <jared@Jareds-MacBook-Pro.local>
2021-02-26 21:25:21 +0800

    updated metadata

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <br>
+        <div class="columns">
+            <div class="column">
+
+            </div>
+            <div class="column">
+                <div class="card">
+                    <div class="card-content">
+                        <form>
+                        <section>                            
+                            <b-field horizontal label="Username" >
+                                <b-input value="" placeholder="Enter your email"></b-input>
+                            </b-field>
+                            <b-field horizontal label="Password" >
+                                <b-input type="password" value="" placeholder="Enter your password"></b-input>
+                            </b-field>
+                            <b-button class="is-light is-info">Login</b-button><br>
+                            <router-link to="/">Forgot Password?</router-link>                         
+                        </section>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+
+            </div>
+        </div>
+        <br>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
---
+++ src/components/Register.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div class="card">
+            <div class="content">
+                this is registration form 
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
